e651dd4306d5a639606dab5a5c402a205f1c4acb
pcortajarena <p.cortajarena@alumnos.upm.es>
2019-03-13 22:17:32 +0100

    Collecting data from weather API, call API and save results on data/solar-weather-jsons

--- weather_scripts/solarfarms.py
+++ weather_scripts/solarfarms.py
@@ -4,52 +4,50 @@ import calendar
 from dateparser import parse
 
 API_KEYS=[
-    #'140b934cd2664658b5b154454191203',\
-    '7451a453e56f4848b76134919191303',\
-    '0fc0f26c3b264533aeb135149191303',\
-    '76cfa3225e0945ceb2d135337191303',\
-    'e82e4fd8ba5140a3b99135432191303',\
-    '5d63814a8cf043a9912135534191303',\
-    '0f3b36273439481cb42135704191303',\
-    '784747442a2a475ba7e135813191303',\
-    '534b86c931a94e5ebb8140033191303',\
-    '6a499521111e45ccab0135500191303',\
-    '963aba864e25442191f135732191303',\
-    '880ee119290b455b95d135842191303',\
-    '9e03dfc6f92c48799dd135807191303',\
-    'be844d8013584670885135959191303',\
-    '363c7bcccda14b3caf4135912191303'\
+    '0fc0f26c3b264533aeb135149191303',
+    '76cfa3225e0945ceb2d135337191303',
+    'e82e4fd8ba5140a3b99135432191303',
+    '5d63814a8cf043a9912135534191303',
+    '0f3b36273439481cb42135704191303',
+    '784747442a2a475ba7e135813191303',
+    '534b86c931a94e5ebb8140033191303',
+    '6a499521111e45ccab0135500191303',
+    '963aba864e25442191f135732191303',
+    '880ee119290b455b95d135842191303'
     ]
 
+API_KEYS_COUNTER = 0
 
 def request_api(lat, lon, month, year):
+
     # yyyy-MM-dd
     mainurl = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx'
     tp = 1
     f = 'json'
     startdate = '{year:04d}-{month:02d}-01'.format(year=year, month=month)
     enddate = '{year:04d}-{month:02d}-{day:02d}'.format(year=year, month=month, day=calendar.monthrange(year, month)[1])
-    key = API_KEYS[counter//490]
+    key = API_KEYS[API_KEYS_COUNTER]
     url = '{mainurl}?q={lat:5.3f},{lon:5.3f}&date={startdate}&enddate={enddate}&tp={tp:1d}&format={format}&key={key}'
     url = url.format(mainurl=mainurl,lat=lat, lon=lon, tp=tp, format=f, startdate=startdate, enddate=enddate, key=key)
     print(url)
 
     # Request the data
     try:
-        pass
-        #response = #requests.get(url)
-        #response = json.loads(response.text)
+        response = requests.get(url)
+        response = json.loads(response.text)
     except:
         pass
-    
-    # Save the data on disk
+
+    with open('../data/solar-weather-jsons/{}-{}-{}-{}.json'.format(lat, lon, month, year),
+              'w') as outfile:
+        json.dump(response, outfile)
 
 if __name__ == "__main__":
-    with open('data/PVDAQ_solar_energy_hourly_clean.json') as f:
-        data = json.load(f)
+    with open('../data/PVDAQ_solar_energy_hourly_clean.json') as f:
+        solar_data = json.load(f)
 
     counter = 0
-    for k,v in data.items():
+    for k,v in solar_data.items():
         lat = v['site_latitude']
         lon = v['site_longitude']
         if 'data' not in v:
@@ -62,15 +60,20 @@ if __name__ == "__main__":
         month = start.month
         year = start.year
         while year < end.year or (year == end.year and month <= end.month):
-            request_api(lat, lon, month, year)
             counter += 1
+            if counter > 490:
+                counter = 0
+                API_KEYS_COUNTER += 1
+
+            request_api(lat, lon, month, year)
+
             if month == 12:
                 year += 1
                 month = 1
             else:
                 month += 1
 
-    print(counter)
+            print(counter)
 
 
 
